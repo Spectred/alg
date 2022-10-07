1ae143a49ffc55192fcd2d04acade47664ccbf13
spectred <1505073336@qq.com>
2022-10-07 22:46:14 +0800

    3

--- src/string/无重复字符的最长子串_3.java
+++ src/string/无重复字符的最长子串_3.java
@@ -1,5 +1,6 @@
 package string;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -8,21 +9,23 @@ import java.util.Map;
 /**
  * <a href="https://leetcode.cn/problems/longest-substring-without-repeating-characters/">3. 无重复字符的最长子串</a>
  */
-public class LengthOfLongestSubstring_3 {
+public class 无重复字符的最长子串_3 {
 
     @Test
     public void test() {
         String s = "abcabcbb";
         int i = lengthOfLongestSubstring(s);
-        System.out.println(i);
+        Assert.assertEquals(i, 3);
     }
 
     public int lengthOfLongestSubstring(String s) {
-        int left = 0, right = 0, res = 0;
         Map<Character, Integer> window = new HashMap<>(16);
+        int res = 0, left = 0, right = 0;
+
         while (right < s.length()) {
             char c = s.charAt(right++);
             window.put(c, window.getOrDefault(c, 0) + 1);
+
             while (window.get(c) > 1) {
                 char d = s.charAt(left++);
                 window.put(d, window.getOrDefault(d, 0) - 1);
