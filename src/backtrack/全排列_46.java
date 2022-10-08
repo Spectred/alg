c712be8ad77d5bdbefc714f1e86650ff7153129a
spectred <1505073336@qq.com>
2022-10-08 23:21:09 +0800

    46 全排列

--- src/backtrack/全排列_46.java
+++ src/backtrack/全排列_46.java
@@ -1,24 +1,14 @@
 package backtrack;
 
-import org.junit.Test;
-
 import java.util.LinkedList;
 import java.util.List;
 
 /**
  * <a href="https://leetcode.cn/problems/permutations/">46. 全排列</a>
  */
-public class Permute_46 {
+public class 全排列_46 {
 
-
-    @Test
-    public void test() {
-        int[] nums = {1, 2, 3};
-        List<List<Integer>> permute = new Permute_46().permute(nums);
-        permute.forEach(System.out::println);
-    }
-
-    List<List<Integer>> res = new LinkedList<>();
+    private List<List<Integer>> res = new LinkedList<>();
 
     public List<List<Integer>> permute(int[] nums) {
         LinkedList<Integer> track = new LinkedList<>();
@@ -31,14 +21,12 @@ public class Permute_46 {
             res.add(new LinkedList<>(track));
             return;
         }
+
         for (int i = 0; i < nums.length; i++) {
             if (track.contains(nums[i])) continue;
-
-            track.add(nums[i]);
+            track.addLast(nums[i]);
             backtrack(track, nums);
             track.removeLast();
         }
     }
-
-
 }
