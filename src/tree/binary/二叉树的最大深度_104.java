8a83c5ea297e9035807f0612132ba928343e25bd
spectred <1505073336@qq.com>
2022-10-09 12:51:31 +0800

    104 二叉树的最大深度

--- src/tree/binary/二叉树的最大深度_104.java
+++ src/tree/binary/二叉树的最大深度_104.java
@@ -5,17 +5,10 @@ import struct.TreeNode;
 /**
  * <a href="https://leetcode.cn/problems/maximum-depth-of-binary-tree/">104. 二叉树的最大深度</a>
  */
-public class MaxDepth_104 {
-
+public class 二叉树的最大深度_104 {
 
     public int maxDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        int lm = maxDepth(root.left);
-        int lr = maxDepth(root.right);
-        return Math.max(lm, lr) + 1;
+        if (root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
-
-
 }
