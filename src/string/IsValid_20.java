384af36adf3830da5c80f9f168b0d2b0597f4195
spectred <1505073336@qq.com>
2022-11-21 20:44:52 +0800

    2020202020202020202020202020202020202020

--- src/string/IsValid_20.java
+++ src/string/IsValid_20.java
@@ -4,6 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 
+/**
+ * <a href="https://leetcode.cn/problems/longest-substring-without-repeating-characters/">3. 无重复字符的最长子串</a>
+ */
 public class IsValid_20 {
 
     static Map<Character, Character> map = new HashMap<>();
