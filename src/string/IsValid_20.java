beb0c1e97c535db81361c31fc6fff4e6ae84dc02
spectred <1505073336@qq.com>
2022-11-21 20:40:10 +0800

    20

---
+++ src/string/IsValid_20.java
@@ -0,0 +1,30 @@
+package string;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class IsValid_20 {
+
+    static Map<Character, Character> map = new HashMap<>();
+
+    static {
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
+    }
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            if (map.containsKey(c)) {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                if (map.get(stack.pop()) != c) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
